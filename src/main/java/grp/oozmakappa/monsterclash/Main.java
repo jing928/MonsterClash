38b387ccbc29dd19a21b9592ef60c72784d00ac0
Chenglong Ma <mcl.cn@outlook.com>
2019-03-21 17:09:36 +1100

    Initial project and add log4j2 lib

---
+++ src/main/java/grp/oozmakappa/monsterclash/Main.java
@@ -0,0 +1,12 @@
+package grp.oozmakappa.monsterclash;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger LOG = LogManager.getLogger();
+
+    public static void main(String[] args) {
+
+    }
+}
