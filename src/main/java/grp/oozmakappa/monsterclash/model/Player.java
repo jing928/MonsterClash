7de45f28b66146d1c1095f78534d1a4b2eb9e8ae
Lawrence Abdelmalek <s3656022@student.rmit.edu.au>
2019-03-30 15:45:43 +1100

    Added abstract player and player class and methods associated with player -setName, setPiece, setCurPiece

---
+++ src/main/java/grp/oozmakappa/monsterclash/model/Player.java
@@ -0,0 +1,11 @@
+package grp.oozmakappa.monsterclash.model;
+
+import grp.oozmakappa.monsterclash.model.abstracts.AbstractPlayer;
+
+public class Player extends AbstractPlayer {
+
+	public Player(String id, String name) {
+		super(id, name);
+	}
+	
+}
