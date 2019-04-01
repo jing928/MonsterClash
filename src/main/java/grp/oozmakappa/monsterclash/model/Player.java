8a530c1684bf8f501ccd4bc7d59af18dabe36d75
Lawrence Abdelmalek <s3656022@student.rmit.edu.au>
2019-04-01 15:21:45 +1100

    -Removed AbsractPlayer -Added private and public modifiers to respected fields

--- src/main/java/grp/oozmakappa/monsterclash/model/Player.java
+++ src/main/java/grp/oozmakappa/monsterclash/model/Player.java
@@ -1,11 +1,73 @@
 package grp.oozmakappa.monsterclash.model;
 
-import grp.oozmakappa.monsterclash.model.abstracts.AbstractPlayer;
+import grp.oozmakappa.monsterclash.model.abstracts.Piece;
 
-public class Player extends AbstractPlayer {
-
+public class Player {
+	private String id;
+	private String name;
+	private Piece pieces[];
+	private Piece currentSelectedPiece;
+	
 	public Player(String id, String name) {
-		super(id, name);
+		this.id = id;
+		this.name = name;
+		this.pieces = new Piece[3];
+	}
+	
+	/**
+	 * Used for setting the player name
+	 */
+	
+	public boolean setName(String name) {
+		this.name = name;
+		return true;
+	}
+	/**
+	 * This method should be used when the player is assigning
+	 * their monsters (before game starts)
+	 */
+	public boolean setPiece(Piece piece, int pos) {
+		this.pieces[pos] = piece;
+		return true;
+	}
+	/**
+	 * This method is used when the player is selected on the active
+	 * board a monster to use
+	 * It checks that the selected monster is in their "deck" and
+	 * returns true if it is in the deck
+	 */
+	public boolean setCurPiece(Piece piece) {
+		for (Piece curPiece : this.pieces) {
+			if (curPiece.equals(piece)) {
+				currentSelectedPiece = piece;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns Id
+	 */
+	
+	public String getId() {
+		return id;
+	}
+	
+	/**
+	 * Returns name
+	 */
+	
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns current selected piece
+	 */
+	
+	public Piece getCurPiece() {
+		return currentSelectedPiece;
 	}
 	
 }
