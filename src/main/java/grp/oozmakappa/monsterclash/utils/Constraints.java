2a70b3b8654bd74281dd9cd35a3f0a98357db392
Chenglong Ma <mcl.cn@outlook.com>
2019-04-01 23:05:00 +1100

    Add utils classes

---
+++ src/main/java/grp/oozmakappa/monsterclash/utils/Constraints.java
@@ -0,0 +1,17 @@
+package grp.oozmakappa.monsterclash.utils;
+
+/**
+ * @author Chenglong Ma
+ */
+public class Constraints {
+    public static final int MAX_X = 6;
+    public static final int MAX_Y = 6;
+    public static final int CORNER_X = 4;
+    public static final int CORNER_Y = 4;
+    public static final int BOARD_WIDTH = 800;
+    public static final int BOARD_HEIGHT = 800;
+
+
+    public static final int CELL_X = 60;
+    public static final int PIECE_DIAMETER = 100;
+}
