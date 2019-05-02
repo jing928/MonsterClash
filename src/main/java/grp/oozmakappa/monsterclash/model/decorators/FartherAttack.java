b9c1098ab261854dd4426639f7f3ca03ef38b4a6
Chenglong Ma <mcl.cn@outlook.com>
2019-05-03 00:18:11 +1000

    Fix class name typo

---
+++ src/main/java/grp/oozmakappa/monsterclash/model/decorators/FartherAttack.java
@@ -0,0 +1,22 @@
+package grp.oozmakappa.monsterclash.model.decorators;
+
+import grp.oozmakappa.monsterclash.model.abstracts.Piece;
+
+/**
+ * @author Chenglong Ma
+ */
+public class FartherAttack extends AbstractDecorator implements AttackDecorator {
+    private static final int FACTOR = 2;
+
+    public FartherAttack(Piece piece) {
+        super(piece);
+    }
+
+    @Override
+    public void attack(Piece target) {
+        int range = piece.getAttackRange();
+        int strengthenRange = range * FACTOR;
+        piece.setAttackRange(strengthenRange);
+        piece.attack(target);
+    }
+}
