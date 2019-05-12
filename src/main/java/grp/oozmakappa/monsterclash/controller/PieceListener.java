02032082be6448b22d43a2d990ac67330ebbb167
Chenglong Ma <mcl.cn@outlook.com>
2019-05-12 10:26:05 +1000

    Add piece listener and its states

---
+++ src/main/java/grp/oozmakappa/monsterclash/controller/PieceListener.java
@@ -0,0 +1,105 @@
+package grp.oozmakappa.monsterclash.controller;
+
+import grp.oozmakappa.monsterclash.controller.states.ModeSelectionState;
+import grp.oozmakappa.monsterclash.controller.states.PieceButtonState;
+import grp.oozmakappa.monsterclash.controller.states.RollingState;
+import grp.oozmakappa.monsterclash.model.Team;
+import grp.oozmakappa.monsterclash.model.abstracts.Piece;
+import grp.oozmakappa.monsterclash.model.interfaces.DiceObserver;
+import grp.oozmakappa.monsterclash.view.BoardPanel;
+import grp.oozmakappa.monsterclash.view.CellLabel;
+import grp.oozmakappa.monsterclash.view.PieceButton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * @author Chenglong Ma
+ */
+public class PieceListener extends MouseAdapter implements DiceObserver {
+    private static final Logger LOG = LogManager.getLogger();
+    private final BoardPanel boardPanel;
+    private Point initMouseLocation;
+    private Team currTeam = Team.OozmaKappa;
+    private boolean canMove = true;
+    private PieceButton currButton;
+    private PieceButtonState state;
+
+    public PieceListener(BoardPanel boardPanel) {
+        this.boardPanel = boardPanel;
+        state = RollingState.getInstance();
+    }
+
+    public void setState(PieceButtonState state) {
+        this.state = state;
+    }
+
+    public PieceButton getButton() {
+        return currButton;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (invalidPiece(e)) {
+            return;
+        }
+        initMouseLocation = e.getPoint();
+        state.todo(this);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (invalidPiece(e)) {
+            return;
+        }
+        state.done(this);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if (invalidPiece(e)) {
+            return;
+        }
+        // follow the mouse's movement
+        Point piecePoint = e.getComponent().getLocation();
+        int x = piecePoint.x + e.getX() - initMouseLocation.x;
+        int y = piecePoint.y + e.getY() - initMouseLocation.y;
+        e.getComponent().setLocation(x, y);
+    }
+
+    private boolean invalidPiece(MouseEvent e) {
+        if (!canMove || !(e.getComponent() instanceof PieceButton)) {
+            return true;
+        }
+        currButton = (PieceButton) e.getComponent();
+        Piece piece = currButton.getPiece();
+        if (piece.getTeam() != currTeam) {
+            e.consume();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void valueChanged(int value) {
+        canMove = true;// TODO: state
+        setState(ModeSelectionState.getInstance());
+    }
+
+    /**
+     * Delegates to {@link BoardPanel}
+     *
+     * @param button
+     * @return
+     */
+    public CellLabel getClosestCell(PieceButton button) {
+        return boardPanel.getClosestCell(button);
+    }
+
+    public PieceButton getClosestPiece(PieceButton button) {
+        return boardPanel.getClosestPiece(button);
+    }
+}
---
+++ src/main/java/grp/oozmakappa/monsterclash/controller/states/ActionState.java
@@ -0,0 +1,65 @@
+package grp.oozmakappa.monsterclash.controller.states;
+
+import grp.oozmakappa.monsterclash.controller.PieceListener;
+import grp.oozmakappa.monsterclash.model.abstracts.Piece;
+import grp.oozmakappa.monsterclash.view.AbilityDialog;
+import grp.oozmakappa.monsterclash.view.PieceButton;
+
+import java.awt.*;
+
+/**
+ * @author Chenglong Ma
+ */
+public class ActionState implements PieceButtonState {
+    private static ActionState instance;
+    private Point initPieceLocation;
+
+
+    private ActionState() {
+        // for singleton pattern
+    }
+
+    public static ActionState getInstance(Piece piece) {
+        if (instance == null) {
+            instance = new ActionState();
+        }
+        new AbilityDialog(piece).display();
+        if (piece.getCurrAbility() != null) {
+            piece.notifyAttacking();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void todo(PieceListener ctrl) {
+        PieceButton button = ctrl.getButton();
+        Piece piece = button.getPiece();
+        if (piece.getCurrAbility() == null) {
+            new AbilityDialog(piece).display();
+            return;
+        }
+        piece.notifyAttacking();
+        button.addMouseMotionListener(ctrl);
+        initPieceLocation = button.getLocation();
+        // TODO implement strategy pattern
+    }
+
+    @Override
+    public void done(PieceListener ctrl) {
+        PieceButton button = ctrl.getButton();
+        Piece piece = button.getPiece();
+        PieceButton targetButton = ctrl.getClosestPiece(button);
+        PieceButtonState nextState = null;
+        if (targetButton != null) {
+            Piece target = targetButton.getPiece();
+            piece.attack(target);
+            nextState = RollingState.getInstance();
+        } else {
+            nextState = this;
+        }
+        button.removeMouseMotionListener(ctrl);
+        button.setLocation(initPieceLocation);
+        ctrl.setState(nextState);
+    }
+}
