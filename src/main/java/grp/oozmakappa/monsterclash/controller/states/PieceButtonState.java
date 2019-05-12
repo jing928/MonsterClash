e114dbe11a66800307bd5ff63f9ceec7dba9a12d
Chenglong Ma <mcl.cn@outlook.com>
2019-05-12 10:26:05 +1000

    Add piece listener and its states

---
+++ src/main/java/grp/oozmakappa/monsterclash/controller/states/PieceButtonState.java
@@ -0,0 +1,12 @@
+package grp.oozmakappa.monsterclash.controller.states;
+
+import grp.oozmakappa.monsterclash.controller.PieceListener;
+
+/**
+ * @author Chenglong Ma
+ */
+public interface PieceButtonState {
+    void todo(PieceListener ctrl);
+
+    void done(PieceListener ctrl);
+}
