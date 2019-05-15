adbe07b11b894441d43b43a3725b5c53e353d7cb
Jing Li <lyovse@gmail.com>
2019-05-15 15:01:48 +1000

    Add mode change cmd

---
+++ src/main/java/grp/oozmakappa/monsterclash/model/command/ModeChangeCommand.java
@@ -0,0 +1,27 @@
+package grp.oozmakappa.monsterclash.model.command;
+
+import grp.oozmakappa.monsterclash.model.abstracts.Piece;
+import grp.oozmakappa.monsterclash.model.strategies.modes.Mode;
+
+public class ModeChangeCommand implements Command {
+    private final Piece piece;
+    private final Mode newMode;
+    private final Mode prevMode;
+
+    public ModeChangeCommand(Piece piece, Mode newMode) {
+        this.piece = piece;
+        this.newMode = newMode;
+        this.prevMode = piece.getCurrMode();
+    }
+
+    @Override
+    public void execute() {
+        piece.setMode(newMode);
+    }
+
+    @Override
+    public void undo() {
+        piece.setMode(prevMode);
+    }
+    
+}
