bd2f53c1a5ceecb2a8f33d30ebd532f082714ac3
Jing Li <lyovse@gmail.com>
2019-05-16 20:27:36 +1000

    Change deque to array to allow index accessing a specific version

--- src/main/java/grp/oozmakappa/monsterclash/model/immutable/MutableHistory.java
+++ src/main/java/grp/oozmakappa/monsterclash/model/immutable/MutableHistory.java
@@ -2,33 +2,32 @@ package grp.oozmakappa.monsterclash.model.immutable;
 
 import grp.oozmakappa.monsterclash.model.command.Command;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 public class MutableHistory implements History {
     private LinkedList<Command> currentHistory;
-    private Deque<ImmutableHistory> versions;
+    private List<ImmutableHistory> versions;
+    private int versionNum; // Can be used as index
 
     public MutableHistory() {
         this.currentHistory = new LinkedList<>();
-        this.versions = new ArrayDeque<>();
+        this.versions = new ArrayList<>();
         this.versionNum = -1; // Initialize as negative
     }
 
     @Override
     public synchronized void add(Command newCmd) {
         // Create immutable history with transformer and save to versions stack
-        ImmutableHistory next = new ImmutableHistory(Transformer.add(currentHistory, newCmd));
-        versions.push(next);
+        ImmutableHistory next = new ImmutableHistory(++versionNum, Transformer.add(currentHistory, newCmd));
+        versions.add(next);
         // Modify current history so it mirrors the most recent version
         currentHistory.add(newCmd);
     }
 
     @Override
     public synchronized Command removeLast() {
-        ImmutableHistory next = new ImmutableHistory(Transformer.removeLast(currentHistory));
-        versions.push(next);
+        ImmutableHistory next = new ImmutableHistory(++versionNum, Transformer.removeLast(currentHistory));
+        versions.add(next);
         return currentHistory.removeLast();
     }
 
