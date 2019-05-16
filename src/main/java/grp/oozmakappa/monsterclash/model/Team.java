aadfc4e5ae73ad59c04702ef540ab38b25ff5dbf
Chenglong Ma <mcl.cn@outlook.com>
2019-05-17 00:57:32 +1000

    Add lose conditions

--- src/main/java/grp/oozmakappa/monsterclash/model/Team.java
+++ src/main/java/grp/oozmakappa/monsterclash/model/Team.java
@@ -2,5 +2,9 @@ package grp.oozmakappa.monsterclash.model;
 
 public enum Team {
     OozmaKappa,
-    RoarOmegaRoar
+    RoarOmegaRoar;
+
+    public static Team getRivalTeam(Team team) {
+        return team == OozmaKappa ? RoarOmegaRoar : OozmaKappa;
+    }
 }
